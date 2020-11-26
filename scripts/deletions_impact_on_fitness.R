075e9236c2020a02d0cd9ab9f428aa3114438208
borauyar <bora.uyar@mdc-berlin.de>
2020-11-26 16:06:43 +0100

    include F1 in plots

--- scripts/deletions_impact_on_fitness.R
+++ scripts/deletions_impact_on_fitness.R
@@ -170,8 +170,8 @@ get_plots <- function(dt, samples_analysed, sample_order = c('F2', 'F3', 'F4', '
 # Subset deletions for those that exist in F2. Check the number of deletions that still exist in F3, F4, and F5 - 
 # categorized by the deletions' overlap with important sites. 
 # 'no_overlap' serves as a control: This is the category of deletions thatdon't overlap any of the other sites. 
-plots <- get_plots(dt = deletions, samples_analysed = samples_analysed, reference_generation = 'F2', 
-                   sample_order = c('F2', 'F3', 'F4', 'F5'), readSupportThreshold = 0, sites = sites)
+plots <- get_plots(dt = deletions, samples_analysed = samples_analysed, reference_generation = 'F1', 
+                   sample_order = c('F1', 'F2', 'F3', 'F4', 'F5'), readSupportThreshold = 0, sites = sites)
 
 lapply(names(plots), function(analysis) {
   ggsave(filename = paste0("deletions_over_generations.",analysis,'.pdf'), 
